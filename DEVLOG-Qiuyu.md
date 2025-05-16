fe138f6f062f2f283d493aa423f1965b0b7663ae
Leo <qye50@stuy.edu>
2025-05-16 14:43:34 -0400

    working on padding

--- DEVLOG-Qiuyu.md
+++ DEVLOG-Qiuyu.md
@@ -8,5 +8,7 @@ This document must be updated daily every time you finish a work session.
 we talked about which idea we were going to do and how we were going to do it. made the repo and edited the devlog
 
 ### 2025-05-15 - 
-working on making the padding algorithm for md5
+started working on making the padding algorithm for md5
 
+### 2025-05-16 -
+worked on the padding algorithm and printing the binary out to verify everything is right
--- padding.c
+++ padding.c
@@ -3,7 +3,34 @@
 #include <string.h>
 #include <stdint.h>
 
-int main(){
+//prints char in binary
+void print_bin_char(unsigned char c){
+  for(int i = 7; i >= 0; i--) {
+      printf( "%d", ( c >> i ) & 1 ? 1 : 0 );
+  }
+}
 
+//prints str in binary using print_bin_char
+void print_bin_str(unsigned char * str){
+  // for (int i = 0; i < strlen(str); i++){
+  for (int i = 0; str[i] != '\0'; i++){
+    print_bin_char(str[i]);
+    //ternary basically if else
+    (i == strlen(str) - 1) ? printf("") : printf(" ");
+  }
+}
+
+int main(){
+  unsigned char c = 'a';
+  // print_bin_char(c);
+  // printf("\n");
+  char * str = "md5_test";
+  print_bin_str(str);
+  printf("\n");
+  //if password has null character, wont work correctly
+  uint32_t bits = strlen(str) * 8;
+  uint32_t pad = bits % 512 - 1;
+  size_t len = ((strlen(str) + pad + 1) + 8);
+  unsigned char *padded = malloc(len);
 
 }
