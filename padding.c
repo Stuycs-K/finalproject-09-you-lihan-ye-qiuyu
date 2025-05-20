833458c215cc9df6021091f662be9d6e46471c34
Leo <qye50@stuy.edu>
2025-05-20 06:00:01 -0400

    finished padding

--- padding.c
+++ padding.c
@@ -38,11 +38,11 @@ int main(){
   unsigned char c = 'a';
   // print_bin_char(c);
   // printf("\n");
-  char * str = "md5_testmd5_testmd5_testmd5_testmd5_testmd5_testmd5_testmd5_testm";
+  char * str = "md5_testmd5_testmd5_testmd5_testmd5_testmd5_testmd5_testmd5_test";
   print_bin_str(str);
   printf("\n");
   //if password has null character, wont work correctly
-  int bits = strlen(str) * 8;
+  uint64_t bits = strlen(str) * 8;
   printf("%d\n", bits);
   int temp = ((448 - (bits % 512)) + 512) % 512;
   printf("%d\n", temp);
@@ -50,10 +50,26 @@ int main(){
   int size = bits + temp + 64;
   uint32_t *output = (uint32_t *)malloc(bits + temp + 64);
   printf("%d\n", bits + temp + 64);
-  for (int i = 0; i < size / 8; i++) {
-    // printf("%d\n", i);
-    output[i] = 0b0;
+  for (int i = 0; i < bits/32; i++){
+    output[i] = str[i] << 24 | str[i + 1] << 16 | str[i + 2] << 8 | str[i + 3];
   }
+  if (bits % 32){
+    printf("%d\n", bits % 32);
+    output[bits/32] = 0b0;
+  }
+  int n = 0;
+  for (int i = 0; i < (bits % 32)/8; i++){
+    output[bits/32] = output[bits/32] | str[bits/32 + i] << (24 - i * 8);
+    n = i + 1;
+  }
+  output[bits/32] = output[bits/32] | 1 << (7 + (24 - 8 * n));
+  for (int i = bits/32 + 1; i < (size / 32 - 2); i++) {
+    // printf("%d\n", i);
+    output[i] = 128;
+  }
+  printf("%d\n", bits);
+  output[size/32 - 2] = bits >> 32;
+  output[size/32 - 1] = (uint32_t)bits;
   print_bits(output, size/32);
 
 }
